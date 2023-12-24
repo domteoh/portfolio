6f2f2001d088e39aeaab68901f6c0f0bc4a8f6e3
domteoh <domteoh@gmail.com>
2023-12-24 09:53:05 +0800

    Updated Photos; Pinhole test

---
+++ pinhole.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+$('.nav-icon').click(function(){
+  $('#nav-menu-mobile').slideToggle(250);
+});
+
+});
+
+
+// 360 Image Viewer
+
+  window.addEventListener('load', onVrViewLoad);
+
+    function onVrViewLoad() {
+      // Selector '#vrview' finds element with id 'vrview'.
+      var vrView = new VRView.Player('#vrview1', {
+        image: 'https://raw.githubusercontent.com/domteoh/domteoh/master/img/360_2.jpeg',
+        height: '400px'
+      });
+
+      var vrView = new VRView.Player('#vrview2', {
+        image: 'https://raw.githubusercontent.com/domteoh/domteoh/master/img/360_3.jpeg',
+        height: '400px'
+      });
+    }
+
+    $('.image1').on('click', () => {
+      $('.VRcontainer1').fadeIn();
+      $('.VRcontainer1').siblings().hide();
+    });
+
+    $('.image2').on('click', () => {
+      $('.VRcontainer2').fadeIn();
+      $('.VRcontainer2').siblings().hide();
+    });
+
+    $('.image3').on('click', () => {
+      $('.VRcontainer3').fadeIn();
+      $('.VRcontainer3').siblings().hide();
+    });
